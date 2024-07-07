241b26d0ef6f8265a0c1119c8970b1632630691d
ChangDaze <explision000@gmail.com>
2024-07-08 00:58:25 +0800

    [feature]啟用cors設定

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using QuestionBank.Middlewares;
 using QuestionBank.Repositories;
 using QuestionBank.Services;
 using Serilog;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace QuestionBank
 {
@@ -18,11 +19,13 @@ namespace QuestionBank
             IDGeneraterService.SetQuestionID("Server=localhost;Database=question_bank;User Id=postgres;Password=postgres");
             //Add server cache
             builder.Services.AddMemoryCache();
+            //啟用CORS
+            builder.Services.AddCors();
             //Add controller automatic reply deal
             builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
             {
                 options.InvalidModelStateResponseFactory = actionContext => new BadRequestObjectResult(new { Message = "Model binding occurs problem." });
-            });
+            });            
             //Dapper
             builder.Services.AddScoped<IQuestionBankRepository, PgQuestionBankRepository>();
             builder.Services.AddScoped<IQuestionsService, QuestionsService>();
@@ -30,8 +33,10 @@ namespace QuestionBank
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            
             var app = builder.Build();
+
+            
             // 在中介程序中全域處理例外
             app.UseExceptionHandleMiddleware();
             // Configure the HTTP request pipeline.
@@ -43,7 +48,13 @@ namespace QuestionBank
 
 
             app.UseHttpsRedirection();
-
+            app.UseCors(
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                }
+            ); //UseCors 的呼叫必須放在 UseRouting 之後(沒有寫出UseRouting好像沒差)、UseAuthorization 之前
+               //用swagger+get會看不出header，可能要用test - cors.org之類的測試比較方便
             app.UseAuthorization();
 
 
